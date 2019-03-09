2ec97536615a8d5eb019f943a8253563ac0b7b9b
Mohamed Daw <daw@aucegypt.edu>
2019-03-10 01:51:01 +0200

    g

--- seconds.c
+++ seconds.c
@@ -44,8 +44,8 @@ size_t count, loff_t *pos)
     }
     completed = 1;
 
-    elapsed_time = jiffies/ HZ - INITIAL_JIFFIES/ HZ ;
-    rv = sprintf(buffer, "Elapsed Time on Module Init: %lu\n%lu\n", jiffies/ HZ , INITIAL_JIFFIES/ HZ);
+    elapsed_time = jiffies/(HZ * 1000) - INITIAL_JIFFIES/(HZ * 1000);
+    rv = sprintf(buffer, "Elapsed Time on Module Init: %d\n", elapsed_time);
 /* copies kernel space buffer to user space usr buf */             
     copy_to_user(usr_buf, buffer, rv);
     return rv;
