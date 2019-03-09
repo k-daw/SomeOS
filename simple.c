59429d9ee6707c384bd20f249d2f078262fce36a
Mohamed Daw <daw@aucegypt.edu>
2019-03-09 03:42:47 +0200

    Updated

--- simple.c
+++ simple.c
@@ -1,20 +1,21 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/hash.h>
-#include <linux/gcd.h> 
+#include <asm/param.h>
+#include <linux/jiffies.h>
 
 /* This function is called when the module is loaded. */
 int simple_init(void)
 {       
-    printk(KERN_INFO "GOLDEN RATIO: %lu\n", GOLDEN_RATIO_PRIME); 
-    printk(KERN_INFO "GDC: %lu\n", gcd(3300,24)); 
+    printk(KERN_INFO "HZ :  %lu\n", HZ); 
+    printk(KERN_INFO "jiffies: %lu\n", jiffies); 
     return 0;
 }
 
 void simple_exit(void)
 {
     printk(KERN_INFO "Removing Kernel Module∖n"); 
+    printk(KERN_INFO "jiffies: %lu\n", jiffies);
 }
 
 module_init(simple_init);
