ed2e094839623c3372b8bf7d17936c1d1e911d93
Mohamed Daw <daw@aucegypt.edu>
2019-03-10 00:38:04 +0200

    IMP

--- jiffies.c
+++ jiffies.c
@@ -1,7 +1,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h> 
 #include <linux/module.h> 
-#include <linux/proc fs.h> 
+#include <linux/proc_fs.h> 
 #include <asm/uaccess.h>
 
 #define BUFFER_SIZE 128 
