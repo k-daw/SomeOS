9e02cb8140699dfcdb940d44cf7135909fae4632
Mohamed Daw <daw@aucegypt.edu>
2019-03-09 03:27:37 +0200

    Update simple.c

--- simple.c
+++ simple.c
@@ -2,12 +2,19 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 
-/* This function is called when the module is loaded. */ int simple init(void)
-{       (KERN INFO "Loading Kernel Module∖n"); return 0;
+/* This function is called when the module is loaded. */
+int simple init(void)
+{       (KERN INFO "Loading Kernel Module∖n"); 
+return 0;
 }
-/* This function is called when the module is removed. */ void simple exit(void)
-{       (KERN INFO "Removing Kernel Module∖n"); }
-/* Macros for registering module entry and exit points. */            (simple init);
-           (simple exit);
-MODULE LICENSE("GPL");
-MODULE DESCRIPTION("Simple Module"); MODULE AUTHOR("SGG");
+
+void simple exit(void)
+{
+    (KERN INFO "Removing Kernel Module∖n"); 
+}
+
+module_init(simple init);
+module_exit(simple exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
