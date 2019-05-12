dbf0daf0d6da1544cd3c631affc5d46f02d4a89d
Mohamed Daw <daw@aucegypt.edu>
2019-05-12 06:07:46 +0200

    Added More functionality

--- STA/sleeping_ta2.c
+++ STA/sleeping_ta2.c
@@ -111,7 +111,7 @@ int insert(int student_id)
     
     if (waiting_count == MAX)
     {    
-        printf("WAITING IS FULL, STUDENT %d is returning back\n", student_id);
+        //printf("WAITING IS FULL, STUDENT %d is returning back\n", student_id);
         return 0;  // Addition Failed
     }
     else
